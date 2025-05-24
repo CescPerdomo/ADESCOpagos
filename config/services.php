64986f42af730457417253607b2b05c8c6f5738b
CodeSandbox <support@codesandbox.io>
2025-05-24 16:16:13 +0000

    config: agregar credenciales de PayPal

--- config/services.php
+++ config/services.php
@@ -1,45 +1,27 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+    "mailgun" => [
+        "domain" => env("MAILGUN_DOMAIN"),
+        "secret" => env("MAILGUN_SECRET"),
+        "endpoint" => env("MAILGUN_ENDPOINT", "api.mailgun.net"),
+        "scheme" => "https",
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    "postmark" => [
+        "token" => env("POSTMARK_TOKEN"),
     ],
 
-    'resend' => [
-        'key' => env('RESEND_KEY'),
+    "ses" => [
+        "key" => env("AWS_ACCESS_KEY_ID"),
+        "secret" => env("AWS_SECRET_ACCESS_KEY"),
+        "region" => env("AWS_DEFAULT_REGION", "us-east-1"),
     ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
+    "paypal" => [
+        "client_id" => env("PAYPAL_CLIENT_ID", "AZU65mUaDihHkkAH_57epB1l3HXwds7YRFc8Fmy3hgTe4ahGIK-Cbi6RejmvuDRNwTBu6RS58qL1GNzh"),
+        "secret" => env("PAYPAL_SECRET", "EMGCETnlccZxIy8tt9tq0r8iy1zFOql19dFiq5x04ChRKEB0zZoeyPgJE6PFY26r7TohO_nBQDR2BRI6"),
+        "mode" => env("PAYPAL_MODE", "sandbox"),
+        "currency" => env("PAYPAL_CURRENCY", "USD"),
     ],
-
-    'paypal' => [
-        'client_id' => env('PAYPAL_CLIENT_ID'),
-        'secret' => env('PAYPAL_SECRET'),
-        'mode' => env('PAYPAL_MODE', 'sandbox'),
-        'currency' => env('PAYPAL_CURRENCY', 'USD'),
-    ],
-
 ];
