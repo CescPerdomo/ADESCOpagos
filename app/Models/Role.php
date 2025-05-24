b590c8460654989b0b357567471a6679dbfd89ce
CodeSandbox <support@codesandbox.io>
2025-05-24 16:00:57 +0000

    docs: agregar documentación al modelo Role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,13 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Modelo de Roles
+ * 
+ * Este modelo gestiona los roles de usuario en el sistema.
+ * 
+ * Puntos de Integración:
+ * - Agregar nuevos roles mediante el seeder RoleSeeder
+ * - Extender con permisos específicos
+ * - Implementar niveles de acceso personalizados
+ * 
+ * Relaciones:
+ * - users: Relación muchos a muchos con el modelo User
+ * 
+ * @property int $id ID del rol
+ * @property string $name Nombre del rol
+ * @property string $description Descripción del rol
+ * @property \Carbon\Carbon $created_at Fecha de creación
+ * @property \Carbon\Carbon $updated_at Fecha de actualización
+ */
 class Role extends Model
 {
+    /**
+     * Los atributos que se pueden asignar masivamente.
+     *
+     * @var array<string>
+     */
     protected $fillable = [
-        'name',
-        'slug'
+        "name",
+        "description"
     ];
 
+    /**
+     * Obtiene los usuarios que tienen este rol.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function users()
     {
         return $this->belongsToMany(User::class);
